fc6a224d49ffd95d69b5f9b35f95004a4d887522
Aleksander Kruk <kruk.aleksander@hotmail.com>
2022-03-28 19:26:45 +0200

    change endpoint, empty search is sending random users and repos

---
+++ pages/api/search.ts
@@ -0,0 +1,103 @@
+import type { NextApiRequest, NextApiResponse } from "next";
+const { createTokenAuth } = require("@octokit/auth-token");
+
+const { request } = require("@octokit/request");
+let arrayOfReadyClients = [];
+
+export default async function searchUser(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+  const searchString = req.query.search;
+  const auth = createTokenAuth("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");
+  const authentication = await auth();
+
+  const requestWithAuth = request.defaults({
+    request: {
+      hook: auth.hook,
+    },
+  });
+  if (searchString.length < 1) {
+    const dataUsers = await requestWithAuth("GET /users").then(
+      (data: { data: any }) => {
+        return data.data;
+      }
+    );
+    const userData = await Promise.all(
+      dataUsers.map(async (client: any) => {
+        const { login } = client;
+        let { data: responseUser } = await requestWithAuth(
+          `GET /users/${login}`
+        );
+        return responseUser;
+      })
+    );
+
+    const dataRepos = await requestWithAuth("GET /repositories").then(
+      (data: { data: any }) => {
+        return data.data;
+      }
+    );
+    const repoData = await Promise.all(
+      dataRepos.map(async (repo: any) => {
+        const { id } = repo;
+        let { data: responseRepo } = await requestWithAuth(
+          `GET /repositories/${id}`
+        );
+        return responseRepo;
+      })
+    );
+
+    const userDataMapped = userData.map((user: any) => {
+      return {
+        id: user.id,
+        login: user.login,
+        name: user.name,
+        followers: user.followers,
+        following: user.following,
+        location: user.location,
+        avantar: user.avatar_url,
+        type: "user",
+      };
+    });
+    const repoDataMapped = repoData.map((user: any) => {
+      return {
+        id: user.id,
+        full_name: user.full_name,
+        description: user.description,
+        stars: user.stargazers_count,
+        languages: user.language,
+        updated_on: user.updated_at,
+        type: "repo",
+      };
+    });
+    return res.status(200).json(
+      [...userDataMapped, ...repoDataMapped].sort(function (a, b) {
+        return a.id - b.id;
+      })
+    );
+  } else {
+    return res.status(200).json({ searching: searchString });
+  }
+}
+
+interface PersonData {
+  id: number;
+  login: string;
+  name: string;
+  followers: number;
+  following: number;
+  location: string;
+  avatar: string;
+  type: "user";
+}
+
+interface RepoData {
+  id: number;
+  full_name: string;
+  description: string;
+  stars: number;
+  languages: {};
+  updated_on: string;
+  type: "repo";
+}
