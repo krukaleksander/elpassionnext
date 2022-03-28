265807c70115197b34db90b2ac71b34ff949c15d
Aleksander Kruk <kruk.aleksander@hotmail.com>
2022-03-28 11:15:31 +0200

    send random users

--- pages/api/users.ts
+++ pages/api/users.ts
@@ -1,5 +1,38 @@
 import type { NextApiRequest, NextApiResponse } from "next";
+const { request } = require("@octokit/request");
 
-export default function handler(req: NextApiRequest, res: NextApiResponse) {
-  res.status(200).json({ name: "John Doe" });
+export default async function searchUser(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+  const response = await request("GET /users");
+  const data = response.data;
+
+  return res.status(200).json(
+    data.map((user: any): PersonData => {
+      const { login, name, followers, following, location, avatar_url } = user;
+      return {
+        login,
+        name,
+        followers,
+        following,
+        location,
+        avatar: avatar_url,
+        type: "user",
+      };
+    })
+  );
 }
+
+//zapytać o repozytoria, zapytać o userów, posortować i zwrócić
+interface PersonData {
+  login: string;
+  name: string;
+  followers: number;
+  following: number;
+  location: string;
+  avatar: string;
+  type: "user";
+}
+
+interface repoData {}
