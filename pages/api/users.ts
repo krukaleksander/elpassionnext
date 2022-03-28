b02fc7e07f122c9ed021ec36ac37fa17a443e404
Aleksander Kruk <kruk.aleksander@hotmail.com>
2022-03-28 11:30:53 +0200

    create RepoData interface

--- pages/api/users.ts
+++ pages/api/users.ts
@@ -5,13 +5,18 @@ export default async function searchUser(
   req: NextApiRequest,
   res: NextApiResponse
 ) {
-  const response = await request("GET /users");
-  const data = response.data;
+  const randomResponseUsers = await request("GET /users");
+  const randomResponseRepos = await request("GET /repositories");
+  const randomUsersData = randomResponseUsers.data;
+  const randomReposData = randomResponseRepos.data;
+  console.log(randomReposData);
 
   return res.status(200).json(
-    data.map((user: any): PersonData => {
-      const { login, name, followers, following, location, avatar_url } = user;
+    randomUsersData.map((user: any): PersonData => {
+      const { id, login, name, followers, following, location, avatar_url } =
+        user;
       return {
+        id,
         login,
         name,
         followers,
@@ -26,6 +31,7 @@ export default async function searchUser(
 
 //zapytać o repozytoria, zapytać o userów, posortować i zwrócić
 interface PersonData {
+  id: number;
   login: string;
   name: string;
   followers: number;
@@ -35,4 +41,11 @@ interface PersonData {
   type: "user";
 }
 
-interface repoData {}
+interface RepoData {
+  id: number;
+  full_name: string;
+  description: string;
+  stars: number;
+  language: string;
+  updated_on: string;
+}
