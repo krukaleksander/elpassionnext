520653a33c21c4eb86f1a4d976dad05ce008ccf9
Aleksander Kruk <kruk.aleksander@hotmail.com>
2022-03-28 10:17:29 +0200

    create route http://localhost:3000/api/users

--- pages/api/hello.ts
+++ pages/api/hello.ts
@@ -1,3 +1,5 @@
+//piszę endpoint, który będzie zwracał informacje o wyszukiwanym użytkoniku
+
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import type { NextApiRequest, NextApiResponse } from 'next'
 
