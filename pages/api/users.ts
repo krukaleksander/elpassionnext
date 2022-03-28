33e8c85ac66a4b79b58ff05d8207d16f55de7b7a
Aleksander Kruk <kruk.aleksander@hotmail.com>
2022-03-28 13:18:02 +0200

    working on github auth

--- pages/api/users.ts
+++ pages/api/users.ts
@@ -1,16 +1,31 @@
 import type { NextApiRequest, NextApiResponse } from "next";
+
 const { request } = require("@octokit/request");
 
 export default async function searchUser(
   req: NextApiRequest,
   res: NextApiResponse
 ) {
-  const randomResponseUsers = await request("GET /users");
-  const randomResponseRepos = await request("GET /repositories");
+  const randomResponseUsers = await request("GET /users", {});
+  const randomResponseRepos = await request("GET /repositories/", {
+    repository_id: 42,
+  });
   const randomUsersData = randomResponseUsers.data;
   const randomReposData = randomResponseRepos.data;
-  console.log(randomReposData);
 
+  const randomReposResult = randomReposData.map(async (repo: any) => {
+    const { id, full_name, description, stargazers_url, languages_url } = repo;
+    return {
+      id,
+      full_name,
+      description,
+      stars: 0,
+      languages: { JS: 200 },
+      updated_on: "10.03.2022",
+      type: "repo",
+    };
+  });
+  console.log(randomReposResult);
   return res.status(200).json(
     randomUsersData.map((user: any): PersonData => {
       const { id, login, name, followers, following, location, avatar_url } =
@@ -46,6 +61,7 @@ interface RepoData {
   full_name: string;
   description: string;
   stars: number;
-  language: string;
+  languages: {};
   updated_on: string;
+  type: "repo";
 }
